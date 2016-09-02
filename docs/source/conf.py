08c1801cfa3353f21ea3d191edba36d7956ea286
Tommy Stallings <tommy.stallings@salesforce.com>
2016-09-02 14:52:12 -0700

    Adding mock class for RTD to build sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['geoip']
+MOCK_MODULES = ['GeoIP', 'readline']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
